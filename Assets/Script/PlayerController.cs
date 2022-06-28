3d0f7651a217d300a99cc9680547763b876dd1c5
Fatemeh Mohammadi <63354670+fatemeshahali@users.noreply.github.com>
2022-06-28 18:46:45 +0430

    Add coins and enemies and barrier

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerController : MonoBehaviour
+{
+    public bool IsSit = false;
+    public int currentJumpCount = 0;
+    public bool isGrounded = false;
+    public bool OnceJumpRayCheck = false;
+
+    public bool Is_DownJump_GroundCheck = false;
+    protected float m_MoveX;
+    public Rigidbody2D m_rigidbody;
+    protected CapsuleCollider2D m_CapsulleCollider;
+    public Animator m_Anim;
+
+    [Header("[Setting]")]
+    public float MoveSpeed = 6;
+    public int JumpCount = 2;
+    public float jumpForce = 15f;
+    public GameObject player;
+    public GameObject fire;
+
+    protected void AnimUpdate()
+    {
+        if (!m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                m_Anim.Play("Attack");
+            }
+            else
+            {
+                if (m_MoveX == 0)
+                {
+                    if (!OnceJumpRayCheck)
+                        m_Anim.Play("Idle");
+                }
+                else
+                {
+                    m_Anim.Play("Run");
+                }
+            }
+        }
+    }
+
+    protected void Filp(bool bLeft)
+    {
+        if (bLeft)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+    protected void prefromJump()
+    {
+        m_Anim.Play("Jump");
+        m_rigidbody.velocity = new Vector2(0, 0);
+        m_rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        OnceJumpRayCheck = true;
+        isGrounded = false;
+        currentJumpCount++;
+    }
+    protected void DownJump()
+    {
+        if (!isGrounded)
+            return;
+
+        if (!Is_DownJump_GroundCheck)
+        {
+            m_Anim.Play("Jump");
+            m_rigidbody.AddForce(-Vector2.up * 10);
+            isGrounded = false;
+            m_CapsulleCollider.enabled = false;
+            StartCoroutine(GroundCapsulleColliderTimmerFuc());
+        }
+    }
+    IEnumerator GroundCapsulleColliderTimmerFuc()
+    {
+        yield return new WaitForSeconds(0.3f);
+        m_CapsulleCollider.enabled = true;
+    }
+
+
+    Vector2 RayDir = Vector2.down;
+    float PretmpY;
+    float GroundCheckUpdateTic = 0;
+    float GroundCheckUpdateTime = 0.01f;
+    protected void GroundCheckUpdate()
+    {
+        if (!OnceJumpRayCheck)
+            return;
+
+        GroundCheckUpdateTic += Time.deltaTime;
+
+        if (GroundCheckUpdateTic > GroundCheckUpdateTime)
+        {
+            GroundCheckUpdateTic = 0;
+
+            if (PretmpY == 0)
+            {
+                PretmpY = transform.position.y;
+                return;
+            }
+
+            float reY = transform.position.y - PretmpY;
+
+            if (reY <= 0)
+            {
+                if (isGrounded)
+                {
+                    LandingEvent();
+                    OnceJumpRayCheck = false;
+                }
+                else
+                {
+                  //  Debug.Log("Collision");
+                }
+            }
+            PretmpY = transform.position.y;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Block"))
+        {
+            Instantiate(fire, transform.position, Quaternion.identity);
+            Destroy(player);
+            Debug.Log("OK");
+        }
+    }
+    protected abstract void LandingEvent();
+
+}
---
+++ Assets/Script/Swordman.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swordman : PlayerController
+{
+    public GameObject Camera;
+    private void Start()
+    {
+        m_CapsulleCollider = this.transform.GetComponent<CapsuleCollider2D>();
+        m_Anim = this.transform.Find("model").GetComponent<Animator>();
+        m_rigidbody = this.transform.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        checkInput();
+
+        if (m_rigidbody.velocity.magnitude > 30)
+        {
+            m_rigidbody.velocity = new Vector2(m_rigidbody.velocity.x - 0.1f, m_rigidbody.velocity.y - 0.1f);
+        }
+    }
+    public void checkInput()
+    {
+        Camera.transform.position = new Vector3(transform.position.x, transform.position.y, -2);
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            IsSit = true;
+            m_Anim.Play("Sit");
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            m_Anim.Play("Idle");
+            IsSit = false;
+        }
+
+        if (m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Sit"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (currentJumpCount < JumpCount)
+                {
+                    DownJump();
+                }
+            }
+            return;
+        }
+
+        m_MoveX = Input.GetAxis("Horizontal");
+
+        GroundCheckUpdate();
+
+        if (!m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                m_Anim.Play("Attack");
+            }
+            else
+            {
+                if (m_MoveX == 0)
+                {
+                    if (!OnceJumpRayCheck)
+                        m_Anim.Play("Idle");
+                }
+                else
+                {
+                    m_Anim.Play("Run");
+                }
+            }
+        }
+
+
+        /*   if (Input.GetKey(KeyCode.Alpha1))
+           {
+               m_Anim.Play("Die");
+           }
+        */
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (isGrounded)
+            {
+                if (m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+                    return;
+
+                transform.transform.Translate(Vector2.right * m_MoveX * MoveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.transform.Translate(new Vector3(m_MoveX * MoveSpeed * Time.deltaTime, 0, 0));
+            }
+
+            if (m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+                return;
+
+            if (!Input.GetKey(KeyCode.RightArrow))
+                Filp(false);
+
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (isGrounded)
+                if (m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+                    return;
+
+            transform.transform.Translate(Vector2.right * m_MoveX * MoveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.transform.Translate(new Vector3(m_MoveX * MoveSpeed * Time.deltaTime, 0, 0));
+        }
+
+        if (m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+            return;
+
+        if (!Input.GetKey(KeyCode.LeftArrow))
+            Filp(true);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+                return;
+
+            if (currentJumpCount < JumpCount)
+            {
+                if (!IsSit)
+                {
+                    prefromJump();
+                }
+                else
+                {
+                    DownJump();
+                }
+            }
+        }
+    }
+
+    protected override void LandingEvent()
+    {
+        if (!m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Run") && !m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+            m_Anim.Play("Idle");
+    }
+
+}
+
